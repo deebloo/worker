6dc62e06363b5385eda9635238362e1a2123d63d
Danny Blue <dblue@deloitte.com>
2015-05-16 21:53:03 -0400

    update

---
+++ build/upversion.js
@@ -0,0 +1,55 @@
+var fs = require('fs');
+
+var files = {
+  bower: __dirname + '/../bower.json',
+  package: __dirname + '/../package.json'
+};
+
+var upType = process.argv[2];
+
+fs.readFile(files.bower, function(err, data) {
+  upVersion(files.bower, err, data);
+});
+
+fs.readFile(files.package, function(err, data) {
+  upVersion(files.package, err, data);
+});
+
+function upVersion(writeTo, err, data) {
+  var file, version;
+
+  if(err) { throw err; }
+
+  file = JSON.parse(data);
+
+  version = file.version.split('.');
+
+  version.forEach(function(val, idx) {
+    version[idx] = parseInt(val);
+  });
+
+  switch(upType) {
+    case 'major':
+      version[0]++;
+      version[1] = 0;
+      version[2] = 0;
+      break;
+
+    case 'minor':
+      version[1]++;
+      version[2] = 0;
+      break;
+
+    case 'patch':
+      version[2]++;
+      break;
+  }
+
+  file.version = version.join('.');
+
+  fs.writeFile(writeTo, JSON.stringify(file, null, 2), function(err) {
+    if(err) {
+      return console.log(err);
+    }
+  });
+}
--- update.sh
+++ update.sh
@@ -4,7 +4,9 @@ npm run docs
 npm run test
 npm run uglify
 
+node ./build/upversion.js m$1
+
 git pull
 git add -A
-git commit -m$1
+git commit -m$2
 git push
