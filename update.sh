613b204553cae09c053a37b528c5337c1bdbce71
Danny Blue <dblue@deloitte.com>
2015-05-19 19:22:39 -0400

    update comments

--- update.sh
+++ update.sh
@@ -1,36 +1,23 @@
 #!/usr/bin/env bash
 
-################
-# Run Unit Tests
-################
 echo "########## running unit tests"
 npm run test
 
-#############
-# Uglify Code
-#############
 echo "########## uglify the code"
 npm run uglify
 cp ./src/web-worker.js dist
 
-#########################################################
-# If a second argument is passed in. (major|minor|patch))
-#########################################################
 if [ "$2" ]
 then
-    ###########################################
-    # Increment the version and tag for release
-    ###########################################
     echo "########## increment the release and push the tag to the repo"
     node ./build/up-version.js $2
     git tag -a "$(node ./build/get-version.js)" -m "$1"
     git push origin master --tags
 
-    ########################################
-    # Update the docs to the gh-pages branch
-    ########################################
     echo "########## create the documents"
     npm run docs
+
+    echo "########## copy docs to gh-pages branch and push"
     cd ..
     git clone https://github.com/deebloo/worker.git worker-docs
     cd worker-docs
@@ -48,9 +35,6 @@ then
     rm -rf worker-docs
 fi
 
-######################################
-# Commit and push the code to the repo
-######################################
 echo "########## commit and push the code up to the repo"
 git pull
 git add -A
