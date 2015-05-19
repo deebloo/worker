32d912cf88a3f7b6a68904aad5739fafbd6d0f6e
Danny Blue <dblue@deloitte.com>
2015-05-19 19:20:46 -0400

    update comments

--- update.sh
+++ update.sh
@@ -1,44 +1,58 @@
 #!/usr/bin/env bash
 
-echo "########## create the documents"
-npm run docs
-
+################
+# Run Unit Tests
+################
 echo "########## running unit tests"
 npm run test
 
+#############
+# Uglify Code
+#############
 echo "########## uglify the code"
 npm run uglify
 cp ./src/web-worker.js dist
 
+#########################################################
+# If a second argument is passed in. (major|minor|patch))
+#########################################################
 if [ "$2" ]
 then
-  echo "########## increment the release and push the tag to the repo"
-  node ./build/up-version.js $2
-  git tag -a "$(node ./build/get-version.js)" -m "$1"
-  git push origin master --tags
+    ###########################################
+    # Increment the version and tag for release
+    ###########################################
+    echo "########## increment the release and push the tag to the repo"
+    node ./build/up-version.js $2
+    git tag -a "$(node ./build/get-version.js)" -m "$1"
+    git push origin master --tags
+
+    ########################################
+    # Update the docs to the gh-pages branch
+    ########################################
+    echo "########## create the documents"
+    npm run docs
+    cd ..
+    git clone https://github.com/deebloo/worker.git worker-docs
+    cd worker-docs
+    git checkout gh-pages
+    rm -rf *
+    cd ..
+    cd worker
+    cp -R "./docs/". "../worker-docs/"
+    cd ..
+    cd worker-docs
+    git add -A
+    git commit -m"update gh-pages doc"
+    git push
+    cd ..
+    rm -rf worker-docs
 fi
 
+######################################
+# Commit and push the code to the repo
+######################################
 echo "########## commit and push the code up to the repo"
 git pull
 git add -A
 git commit -m"$1"
 git push
-
-cd ..
-git clone https://github.com/deebloo/worker.git worker-docs
-cd worker-docs
-git checkout gh-pages
-rm -rf *
-
-cd ..
-cd worker
-cp -R "./docs/". "../worker-docs/"
-
-cd ..
-cd worker-docs
-git add -A
-git commit -m"update gh-pages doc"
-git push
-
-cd ..
-rm -rf worker-docs
