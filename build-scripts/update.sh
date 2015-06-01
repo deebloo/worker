c215618257fba4a94aaf99fe84939a3019ac16cf
Danny Blue <dblue@deloitte.com>
2015-06-01 06:30:05 -0400

    rewrite to different style

--- build-scripts/update.sh
+++ build-scripts/update.sh
@@ -11,11 +11,11 @@ cp ../src/worker.js dist
 if [ "$2" ]
 then
     echo "########## increment the release and push the tag to the repo"
-    node up-version.js $2
-    git tag -a "$(node get-version.js)" -m "$1"
+    node ./up-version.js $2
+    git tag -a "$(node ./get-version.js)" -m "$1"
     git push origin master --tags
 
-    gh-pages.sh
+    ./gh-pages.sh
 fi
 
 echo "########## commit and push the code up to the repo"
