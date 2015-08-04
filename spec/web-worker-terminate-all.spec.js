9a1bb328bf9981168b68af961876c18c07eab6e1
Danny Blue <dannybluedesign@gmail.com>
2015-08-04 13:21:48 -0400

    update unit tests

---
+++ spec/web-worker-terminate-all.spec.js
@@ -0,0 +1,33 @@
+describe('web-worker', function() {
+  var workers, myWorker, result;
+
+  beforeEach(function(done) {
+    workers = $worker();
+
+    myWorker = workers.create(function(e) {
+      var foo = [], min = e.data.min, max = e.data.max;
+
+      for (var i = 0; i < e.data.length; i++) {
+        foo.push(Math.floor(Math.random() * (max - min)) + min);
+      }
+
+      self.postMessage(foo);
+    });
+
+    myWorker.onmessage = function(res) {
+      result = res.data;
+
+      done();
+    };
+
+    myWorker.postMessage({length: 1024, min: 0, max: 9999});
+  });
+
+  afterEach(function() {
+    workers.terminate();
+  });
+
+  it('should create a big array', function() {
+    expect(result.length).toBe(1024);
+  });
+});
